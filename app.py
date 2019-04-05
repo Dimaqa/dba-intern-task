aad484348c041711f9431e8d4988060ccc3972f0
Dimaqa <best2010a@ya.ru>
2019-04-05 21:27:22 +0300

    add test mongo method

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from aiohttp import web
-from views import get_mongo, get_rabbit
+from views import get_mongo, get_rabbit, test_mongo
 
 def setup_routes(app):
     app.router.add_get('/api/v1/get_rabbit', get_rabbit)
     app.router.add_get('/api/v1/get_mongo', get_mongo)
+    app.router.add_get('/api/v1/test_mongo', test_mongo)
 
 app = web.Application()
 setup_routes(app)
--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 
+MONGO_PORT = 27017
+
 async def get_rabbit(request):
     with open('install_rabbit.sh') as f:
         body = f.read()
@@ -9,3 +11,15 @@ async def get_mongo(request):
     with open('install_mongo.sh') as f:
         body = f.read()
     return web.Response(body=body, status=200)
+
+async def test_mongo(request):
+    try:
+        data = request.rel_url.query
+        host = data['host']
+        port = int(data.get('port')) if data.get('port') else MONGO_PORT
+    except:
+        data = 'BAD PARAMS'
+        code = 400
+    else:
+        data = host
+    return web.Response(body=data,status=code)
