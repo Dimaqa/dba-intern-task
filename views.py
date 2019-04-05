2a45afc9a6db331d062ed76edeadac55a46cffa8
Dimaqa <best2010a@ya.ru>
2019-04-05 21:25:40 +0300

    return scripts in body

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
 from aiohttp import web
 
 async def get_rabbit(request):
-    return web.Response(text='true',status=200)
+    with open('install_rabbit.sh') as f:
+        body = f.read()
+    return web.Response(body=body, status=200)
 
 async def get_mongo(request):
-    return web.Response(text='true', status=200)
+    with open('install_mongo.sh') as f:
+        body = f.read()
+    return web.Response(body=body, status=200)
