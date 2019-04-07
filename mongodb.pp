8e88bfffce4e8b064280cb1d902d2346c522931e
Dimaqa <best2010a@ya.ru>
2019-04-07 17:58:49 +0300

    fix manifest

--- mongodb.pp
+++ mongodb.pp
@@ -8,17 +8,17 @@ service {'mongodb':
 exec { 'open server to internet':
   command => "sed -i 's/bind_ip.*/bind_ip = 0.0.0.0/g' /etc/mongodb.conf",
   path => '/bin',
-  notify => service['mongodb']
+  notify => Service['mongodb']
 }
 exec { 'add admin user':
   command => "mongo <<EOF
   use admin;
   db.createUser({user:'root', pwd:'secretpwd', roles: [{role:'root', db:'admin'}]})",
   path => '/usr/bin/',
-  notify => service['mongodb']
+  notify => Service['mongodb']
 }
 exec { 'run as replica':
-  command => 'echo replSetName: "rs0" | sudo tee -a /etc/mongodb.conf && echo "rs.initiate()" | /usr/bin/mongo',
-  path => '/usr/bin/',
-  notify => service['mongodb']
+  command => 'echo replSetName: "rs0" | /usr/bin/tee -a /etc/mongodb.conf && echo "rs.initiate()" | /usr/bin/mongo',
+  path => '/bin',
+  notify => Service['mongodb']
 }
