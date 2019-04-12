368323955866365a834b5b354a3d2f927a63ac24
Dimaqa <best2010a@ya.ru>
2019-04-12 17:28:05 +0300

    exec once

--- app/static/replica.pp
+++ app/static/replica.pp
@@ -1,5 +1,6 @@
 exec { 'run as replica':
   command => 'echo replSet="rs0" | /usr/bin/tee -a /etc/mongodb.conf && echo "rs.initiate()" | /usr/bin/mongo',
   path => '/bin',
+  unless => ["cat /etc/mongodb.conf | grep 'replSet='"],
   notify => Service['mongodb']
 }
