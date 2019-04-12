72cacf8fc7ad2342a7decd756bb26ffdc0774b77
Dimaqa <best2010a@ya.ru>
2019-04-12 17:16:09 +0300

    move static files to another folder

--- app/static/install_replica.sh
+++ app/static/install_replica.sh
@@ -1,3 +1,3 @@
 echo 'replSet="rs0"' | sudo tee -a /etc/mongodb.conf
-echo "rs.initiate()" | /usr/bin/mongo
 service mongodb restart
+echo "rs.initiate()" | /usr/bin/mongo
--- app/views.py
+++ app/views.py
@@ -4,17 +4,17 @@ from pymongo import MongoClient
 MONGO_PORT = 27017
 
 async def get_rabbit(request):
-    with open('install_rabbit.sh') as f:
+    with open('./static/install_rabbit.sh') as f:
         body = f.read()
     return web.Response(body=body, status=200)
 
 async def get_mongo(request):
     data = request.rel_url.query
     replica = data.get('replication')
-    with open('mongodb.pp') as f:
+    with open('./static/mongodb.pp') as f:
         body = f.read()
     if replica.lower() == 'true':
-        with open('replica.pp') as f:
+        with open('./static/replica.pp') as f:
             body += f.read()
     return web.Response(body=body, status=200)
 
@@ -22,14 +22,14 @@ async def test_mongo(request):
     try:
         data = request.rel_url.query
         host = data['host']
+        port = data.get('port')
+        # if port isnt specified, using default one
+        port = int(port) if port else MONGO_PORT
     except:
         body = 'BAD PARAMS'
         code = 400
     else:
         code=200
-        port = data.get('port')
-        # if port isnt specified, using default one
-        port = int(port) if port else MONGO_PORT
         client = MongoClient(host=host,port=port)
         try:
             # and here aiohttp pretty useless,
